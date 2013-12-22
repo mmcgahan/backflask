1fa14c9a7da5ed15389df08de65f05fd9abb0575
Michael McGahan <mike.mcgahan@gmail.com>
2013-12-22 17:09:14 -0500

    requirejs setup+routing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-$(document).foundation();
+requirejs.config({
+    baseUrl: '/assets',
+    paths: {
+        'jquery': 'vendor/jquery/jquery',
+        'underscore': 'vendor/lodash/dist/lodash',
+        'backbone': 'vendor/backbone/backbone',
+        'handlebars': 'vendor/handlebars/handlebars.runtime',
+        'foundation': 'vendor/foundation/js/foundation'
+    }
+});
+
+require(['jquery', 'foundation'],
+    function($) {
+        'use strict';
+
+        $(document).foundation();
+    });
